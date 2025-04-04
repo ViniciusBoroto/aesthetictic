016e70e1a2925da04d100e0cb1d54d838a60db2c
honorioRafael <rafaelhonoriofx@gmail.com>
2025-04-03 21:18:04 -0300

    Adicionado suporte a venv.

--- backend/package.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Instalando dependências...
-pip install fastapi
-pip install pydantic
-pip install sqlalchemy
-pip install uvicorn
-echo Dependências instaladas com sucesso!
-pause
--- backend/startup.py
+++
@@ -1,4 +0,0 @@
-import uvicorn
-from main import get_app
-
-uvicorn.run(get_app(), host="127.0.0.1", port=8000)
